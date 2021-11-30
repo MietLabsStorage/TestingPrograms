513521c01cc7ba503745225a34ce85fb69fca806
mJaJksJ <maksim.m00@mail.ru>
2021-11-30 11:35:49 +0300

    Тестирование поиска

--- WebTesting/WebTesting/SearchTests.cs
+++ WebTesting/WebTesting/SearchTests.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace WebTesting
@@ -11,18 +12,44 @@ namespace WebTesting
         [SetUp]
         public void Setup()
         {
-            WebDriver.SetUp("https://yandex.ru/");
+            WebDriver.SetUp("https://jut.su/");
+            (WebDriver.Driver.Manage().Window).Size = new System.Drawing.Size(1200, 1000);
         }
-        private static By SearchInput => By.Id("text");
-        private static By SearchButton => By.XPath("body > div.body__wrapper > div.container.rows > div.row.rows__row.rows__row_main > div > div.container.container__search.container__line > aqwf > div > div > div > div.home-arrow__search > form > div.search2__button > button");
-        private static By NotFound => By.CssSelector("/html/body/div[1]/div[2]/div[3]/div/div[3]/dqs/div/div/div[1]/div[2]/form/div[2]/button");
+        private static By SearchInput => By.XPath("//*[@id='search_b']/form/input[2]");
+        private static By SearchButton => By.CssSelector("#search_b > form > input[type=submit]:nth-child(3)");
 
-        [TestCase("`", "По вашему запросу ничего не нашлось")]
-        public void Search_BadRequest_NotFound(string input ,string excpected)
+        [TestCase("Шаман", "shamanking/")]
+        [TestCase("Шаман король", "shamanking/")]
+        [TestCase("Шаман кинг", "shamanking/")]
+        [TestCase("Король шаманов", "shamanking/")]
+        [TestCase("шаман", "shamanking/")]
+        [TestCase("шаман король", "shamanking/")]
+        [TestCase("шаман кинг", "shamanking/")]
+        [TestCase("король шаманов", "shamanking/")]
+        [TestCase("shaman king", "shamanking/")]
+        [TestCase("shaman", "shamanking/")]
+        [TestCase("Shaman king", "shamanking/")]
+        [TestCase("Shaman", "shamanking/")]
+        [TestCase("Король", "shamanking/")]
+        [TestCase("King", "shamanking/")]
+        public void Search_AnyNames_FoundPage(string input, string excpectedAddedUrl)
         {
+            var excpected = $"{WebDriver.Driver.Url}{excpectedAddedUrl}";
             WebDriver.GetWaitedElement(SearchInput).SendKeys(input);
             WebDriver.GetWaitedElement(SearchButton).Click();
-            var actual = WebDriver.GetWaitedElement(NotFound).Text;
+            var actual = WebDriver.Driver.Url;
+
+            Assert.AreEqual(excpected, actual);
+        }
+
+        [TestCase("Шаманий король", "search")]
+        [TestCase("Король-шаман", "search")]
+        public void Search_AnyBadNames_NotFoundPage(string input, string excpectedAddedUrl)
+        {
+            var excpected = $"{WebDriver.Driver.Url}{excpectedAddedUrl}";
+            WebDriver.GetWaitedElement(SearchInput).SendKeys(input);
+            WebDriver.GetWaitedElement(SearchButton).Click();
+            var actual = WebDriver.Driver.Url.Substring(0, 21);
 
             Assert.AreEqual(excpected, actual);
         }
