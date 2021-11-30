e4b3d2d6a9c9fd434f7d117047162c53fdeb5488
mJaJksJ <maksim.m00@mail.ru>
2021-11-30 22:11:05 +0300

    видео тест

--- WebTesting/WebTesting/SurveyTests.cs
+++ WebTesting/WebTesting/SurveyTests.cs
@@ -15,12 +15,12 @@ namespace WebTesting
             "body > div.ps-layout > div.ps-layout-center.clearfix > div > div.ps-content > div.poll-vote > div.pollservice-poll > div > div:nth-child(2) > div > div.pollservice-ru_question-list > div > div.pollservice-ru_answer-list";
         private static string GetAnswerTextInListPath(int answerNum) => $"div:nth-child({answerNum}) > div.pollservice-ru_answer-label > label";
 
-        private static string GetAnswerRatioInListPath(int answerNum) => $"#pollservice_poll_0_answer_11593{answerNum}";
+        private static By GetAnswerRatioInListPath(int answerNum) => By.CssSelector($"#pollservice_poll_0_answer_11593{answerNum}");
 
-        private static string GetButtonPath => 
-            "body > div.ps-layout > div.ps-layout-center.clearfix > div > div.ps-content > div.poll-vote > div.pollservice-poll > div > div:nth-child(2) > div > div.pollservice-ru_actions > div > span > div";
+        private static By GetButtonPath =>
+            By.CssSelector("body > div.ps-layout > div.ps-layout-center.clearfix > div > div.ps-content > div.poll-vote > div.pollservice-poll > div > div:nth-child(2) > div > div.pollservice-ru_actions > div > span > div");
 
-        private static string GetTableAnswersPath => "body > div.ps-layout > div.ps-layout-center.clearfix > div > div.ps-content > div.poll-results > div.tabed.clearfix";
+        private static By GetTableAnswersPath => By.CssSelector("body > div.ps-layout > div.ps-layout-center.clearfix > div > div.ps-content > div.poll-results > div.tabed.clearfix");
 
         [TestCase(0, "Только польза", "/results")]
         [TestCase(1, "Один вред", "/results")]
@@ -32,10 +32,10 @@ namespace WebTesting
 
             var actualAnswer = WebDriver.GetWaitedElement(By.CssSelector($"{GetAnswerListPath} > {GetAnswerTextInListPath(answerNum + 1)}")).Text;
 
-            WebDriver.GetWaitedElement(By.CssSelector(GetAnswerRatioInListPath(answerNum))).Click();
-            WebDriver.GetWaitedElement(By.CssSelector(GetButtonPath)).Click();
+            WebDriver.GetWaitedElement(GetAnswerRatioInListPath(answerNum)).Click();
+            WebDriver.GetWaitedElement(GetButtonPath).Click();
 
-            WebDriver.GetWaitedElement(By.CssSelector(GetTableAnswersPath));
+            WebDriver.GetWaitedElement(GetTableAnswersPath);
             var actualUrl = WebDriver.Driver.Url;
 
             Assert.AreEqual((excpectedAnswer, excpectedUrl), (actualAnswer, actualUrl));
@@ -44,7 +44,7 @@ namespace WebTesting
         [TestCase("Не указан вариант ответа")]
         public void Survey_NonChoisedAns_AlertWarning(string excpected)
         {
-            WebDriver.GetWaitedElement(By.CssSelector(GetButtonPath)).Click();
+            WebDriver.GetWaitedElement(GetButtonPath).Click();
             var actual = WebDriver.Driver.SwitchTo().Alert().Text;
 
             Assert.AreEqual(excpected, actual);
---
+++ WebTesting/WebTesting/VideoTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace WebTesting
+{
+    class VideoTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            WebDriver.SetUp("https://www.youtube.com/");
+        }
+
+        private static By VideoSelect(int row, int num) => By.XPath($"/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row[{row}]/div/ytd-rich-item-renderer[{num}]/div/ytd-rich-grid-media/div[1]/ytd-thumbnail/a");
+        private static By VideoTimer => By.CssSelector("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container > div.ytp-progress-bar");
+
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 1)]
+        [TestCase(2, 2)]
+        public void Video_OpenVideo_VideoPlaying(int row, int num)
+        {
+            WebDriver.GetWaitedElement(VideoSelect(row, num)).Click();
+
+            var timer = WebDriver.GetWaitedElement(VideoTimer);
+
+            var time1 = timer.GetAttribute("aria-valueText");
+
+            Thread.Sleep(3000);
+
+            var time2 = timer.GetAttribute("aria-valueText");
+
+            Assert.AreNotEqual(time1, time2);
+        }
+    }
+}
